b70ada695d8e3b6763e5bad5773d462138a4d84a
2dayevents <2dayeventsapp@gmail.com>
2022-10-06 14:15:41 +0200

    add things

--- dist/index.js
+++ dist/index.js
@@ -5,11 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const functions_1 = __importDefault(require("./functions"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
 app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
+    res.send('hello world!');
+});
+app.get('/ciao', (req, res) => {
+    const x = Number(req.query.x) || 0;
+    const y = Number(req.query.y) || 0;
+    res.send((0, functions_1.default)(x, y).toString());
 });
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
--- index.ts
+++ index.ts
@@ -1,13 +1,22 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import calcArea from "./functions";
 
 dotenv.config();
+
+
 
 const app: Express = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+  res.send('hello world!');
+});
+
+app.get('/ciao', (req: Request, res: Response) => {
+  const x: number = Number(req.query.x) || 0;
+  const y: number = Number(req.query.y) || 0;
+  res.send(calcArea(x, y).toString());
 });
 
 
