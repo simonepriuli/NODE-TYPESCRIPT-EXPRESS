3ac514a715fd360049880105a50bca9ba7720e7a
2dayevents <2dayeventsapp@gmail.com>
2022-10-09 10:47:13 +0200

    merge

---
+++ dist/controllers/controller.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.addUser = exports.getUser = void 0;
+var example_1 = require("./example");
+Object.defineProperty(exports, "getUser", { enumerable: true, get: function () { return example_1.getUser; } });
+var example_2 = require("./example");
+Object.defineProperty(exports, "addUser", { enumerable: true, get: function () { return example_2.addUser; } });
+//in this file we export all the functions in the controller folder
---
+++ dist/functions.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function calcArea(x, y) {
+    return x * y;
+}
+exports.default = calcArea;
+;
